9dd27af65e604af7b47477970e9868e0b17de6f6
Essspressso <hologram1220@gmail.com>
2020-09-28 23:15:17 +0900

    Test

---
+++ build.bat
@@ -0,0 +1,5 @@
+
+
+.\vcpkg\downloads\tools\cmake-3.17.2-windows\cmake-3.17.2-win32-x86\bin\cmake.exe -S ./src -B build -DCMAKE_TOOLCHAIN_FILE="E:\Opengl_tutorial\vcpkg\scripts\buildsystems\vcpkg.cmake"
+
+PAUSE
